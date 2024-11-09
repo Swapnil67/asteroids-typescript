4e0f1188c43b09f95d6d8727bd838790b90747b1
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-11-09 20:25:41 +0530

    Added Player Angle Rotation & Input keyboard logic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import './style.css'
+import { Key, Keyboard } from './keyboard';
+import { Vec2 } from './vec2';
 
 const canvas = document.querySelector<HTMLCanvasElement>('#game')!;
 const ctx = canvas.getContext('2d')!;
+
+const input = new Keyboard(window)
 
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
@@ -10,56 +14,53 @@ const FIXED_DELTA_TIME = 1000 / 120;
 
 const player = {
   angle: 0,
-  pos: {
-    x: 100,
-    y: 100,
-  },
-  vel: {
-    x: 0.2,
-    y: 0
-  }
-};
-
-
+  pos: new Vec2(100, 100),
+  vel: new Vec2(0.2, 0)
+}
 
 function drawPlayer(p: typeof player) {
-  const height = 15;
-  const width = 10;
-  
+  const height = 15
+  const width = 10
+
   const vertices = [
-    {x: height, y: 0},
-    {x: -height, y: -width},
-    {x: -height, y: width},
-  ]
+    new Vec2(height, 0),
+    new Vec2(-height, -width),
+    new Vec2(-height, width),
+  ].map(v => v.rotate(player.angle))
 
   ctx.save()
   ctx.strokeStyle = 'white'
   // * initial points
-  ctx.translate(p.pos.x, p.pos.y);
-  ctx.rotate((player.angle * Math.PI) / 180)
-  ctx.beginPath();
-  const [tip, ...tail] = vertices;
+  ctx.translate(p.pos.x, p.pos.y)
+  ctx.beginPath()
+  const [tip, ...tail] = vertices
   // * Move initial point to
-  ctx.moveTo(tip.x, tip.y);
-  for(let vertex of tail) {
+  ctx.moveTo(tip.x, tip.y)
+  for (let vertex of tail) {
     // console.log(vertex);
-    ctx.lineTo(vertex.x, vertex.y);
+    ctx.lineTo(vertex.x, vertex.y)
   }
-  ctx.closePath();
-  ctx.stroke();
+  ctx.closePath()
+  ctx.stroke()
   ctx.restore()
 }
 
 function clearScreen() {
   ctx.fillStyle = '#222222'
-  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillRect(0, 0, WIDTH, HEIGHT)
 }
 
 function update(p: typeof player, dt: number) {
-  p.angle++
-  // p.pos.x += p.vel.x * dt;
-  // p.pos.y += p.vel.y * dt;
-  // p.pos.x %= WIDTH;
+  if(input.isKeyDown(Key.ArrowLeft)) {
+    p.angle--;
+  } else if(input.isKeyDown(Key.ArrowRight)) {
+    p.angle++;
+  }
+  // console.log(p.angle);
+  
+  p.pos.x += p.vel.x * dt;
+  p.pos.y += p.vel.y * dt;
+  p.pos.x %= WIDTH;
 }
 
 let lag = 0                   // * Accumlation of dt
@@ -67,17 +68,17 @@ let lastTime = 0
 
 function loop(now: number) {
   // console.log("now ", now);
-  let deltaTime = now - lastTime;
+  let deltaTime = now - lastTime
   // console.log("deltaTime ", deltaTime.toFixed(2));
-  lag += deltaTime;
+  lag += deltaTime
   // console.log("lag ", lag.toFixed(2));
-  lastTime = now;
+  lastTime = now
 
   // * update             [Physics & Rendering]
   clearScreen()
-  while(lag >= FIXED_DELTA_TIME) {
+  while (lag >= FIXED_DELTA_TIME) {
     update(player, deltaTime)
-    lag -= FIXED_DELTA_TIME;
+    lag -= FIXED_DELTA_TIME
   }
   drawPlayer(player)
 
